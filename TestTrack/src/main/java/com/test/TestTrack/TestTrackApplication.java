f1ed16c2066e6dfbb6b03fc57dc733fd81683ab9
HtetShine1868 <hetaungshine25@gmail.com>
2025-06-26 21:02:11 +0630

    Initial commit

---
+++ TestTrack/src/main/java/com/test/TestTrack/TestTrackApplication.java
@@ -0,0 +1,13 @@
+package com.test.TestTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTrackApplication.class, args);
+	}
+
+}
---
+++ TestTrack/src/test/java/com/test/TestTrack/TestTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.TestTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
