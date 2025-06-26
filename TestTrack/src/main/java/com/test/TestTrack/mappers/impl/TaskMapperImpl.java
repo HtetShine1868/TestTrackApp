7efffb5c0b1d45b5cb6170451bb28fe960211375
HtetShine1868 <hetaungshine25@gmail.com>
2025-06-27 00:04:04 +0630

    creating the dto for task

---
+++ TestTrack/src/main/java/com/test/TestTrack/mappers/impl/TaskMapperImpl.java
@@ -0,0 +1,34 @@
+package com.test.TestTrack.mappers;
+
+import com.test.TestTrack.domain.dto.TaskDto;
+import com.test.TestTrack.domain.entity.Task;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskMapperImpl implements TaskMapper {
+
+    @Override
+    public Task fromDto(TaskDto taskDto) {
+        return new Task(
+                taskDto.id(),
+                taskDto.title(),
+                taskDto.description(),
+                taskDto.dueDate(),
+                taskDto.status(),
+                taskDto.priority()
+        );
+    }
+
+    @Override
+    public TaskDto toDto(Task task) {
+        return new TaskDto(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                task.getPriority(),
+                task.getStatus()
+
+        );
+    }
+}
